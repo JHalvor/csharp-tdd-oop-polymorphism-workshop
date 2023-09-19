21362d87364e95326fc357f4efc5f06fbbcaf1a6
uerbzr <nigel.sibbert@gmail.com>
2023-09-19 07:13:37 +0100

    update

---
+++ csharp.polymorphism.main/Examples/Shop/Basket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp.polymorphism.main.Examples.Shop.Interfaces;
+
+namespace csharp.polymorphism.main.Examples.Shop
+{
+    public class Basket
+    {
+        private List<IProduct> _products = new List<IProduct>();
+
+        public void AddProduct(IProduct product)
+        {
+            _products.Add(product);
+        }
+
+        public List<IProduct> GetProducts { get { return _products; } }
+
+        public decimal BasketTotal { get { return _products.Sum(x => x.Price); } }
+
+        public void DeleteProduct(int id)
+        {
+            _products.RemoveAll(x => x.Id == id);
+        }
+    }
+}
---
+++ csharp.polymorphism.main/Examples/Shop/Guitar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp.polymorphism.main.Examples.Shop.Interfaces;
+
+namespace csharp.polymorphism.main.Examples.Shop
+{
+    public class Guitar : IProduct
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+
+    }
+}
